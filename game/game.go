7ff0676eb05e97b83d21ed55581552a38c542f69
Nikita <nekitar2006@mail.ru>
2025-12-26 19:45:05 +0300

    full logic of a game

--- game/game.go
+++ game/game.go
@@ -2,29 +2,30 @@ package game
 
 import (
 	"errors"
+	"math/rand"
 
 	"github.com/hakuromi/spy-bot/models"
 )
 
-type Manage struct {
+type Manager struct {
 	Game models.Game
 }
 
-func (m *Manage) NewGame() { // создание игры
+func (m *Manager) NewGame() { // создание игры
 	m.Game = models.Game{
 		Players: []models.Player{},
 		Active:  false,
 	}
 }
 
-func (m *Manage) AddPlayer(p models.Player) error { // добавление пользователя
+func (m *Manager) AddPlayer(p models.Player) error { // добавление пользователя
 	if m.Game.Active {
 		return errors.New("Игра уже началась.")
 	}
 
 	for _, player := range m.Game.Players {
 		if player.ID == p.ID {
-			return errors.New("игрок уже в игре")
+			return errors.New("Игрок уже в игре.")
 		}
 	}
 	m.Game.Players = append(m.Game.Players, p)
@@ -32,14 +33,26 @@ func (m *Manage) AddPlayer(p models.Player) error { // добавление по
 	return nil
 }
 
-func (m *Manage) CanStart() bool { // проверка количества игроков
+func (m *Manager) CanStart() bool { // проверка количества игроков
 	return len(m.Game.Players) >= 3
 }
 
-func (m *Manage) Start() error {
+func (m *Manager) chooseSpy() {
+	num := rand.Intn(len(m.Game.Players))
+	m.Game.SpyID = m.Game.Players[num].ID
+}
+
+func (m *Manager) chooseHero() {
+	num := rand.Intn(len(models.Heroes))
+	m.Game.Hero = models.Heroes[num]
+}
+
+func (m *Manager) Start() error {
 	if !m.CanStart() {
 		return errors.New("нужно минимум 3 игрока")
 	}
+	m.chooseSpy()
+	m.chooseHero()
 	m.Game.Active = true
 	return nil
 }
