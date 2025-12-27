4d196b1bb67bf6e542ec89de9bea2c57b9636c7e
Nikita <nekitar2006@mail.ru>
2025-12-27 18:12:16 +0300

    corner cases

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/hakuromi/spy-bot/bot"
 	"github.com/hakuromi/spy-bot/game"
 )
@@ -12,21 +11,8 @@ func main() {
 	var manager game.Manager
 	manager.NewGame()
 
-	botAPI := bot.Init("8578620255:AAHIQsDmMdA33Kpryao6AOC3U8m7OCQKYiQ")
+	botAPI := bot.Init("")
 	updates := bot.GetUpdates(botAPI)
-
-	commands := []tgbotapi.BotCommand{
-		{Command: "start", Description: "Запуск бота"},
-		{Command: "newgame", Description: "Создать новую игру"},
-		{Command: "join", Description: "Присоединиться к игре"},
-		{Command: "begin", Description: "Начать игру"},
-	}
-
-	config := tgbotapi.NewSetMyCommands(commands...)
-	_, err := botAPI.Request(config)
-	if err != nil {
-		log.Println("Не удалось зарегистрировать команды:", err)
-	}
 
 	log.Println("Бот запущен.")
 
@@ -35,12 +21,18 @@ func main() {
 			switch update.Message.Text {
 			case "/start":
 				bot.HandleStart(botAPI, update.Message.Chat.ID)
-			case "/newgame":
-				bot.HandleNewGame(botAPI, update.Message.Chat.ID, &manager)
-			case "/join":
+			case "[dota] создать игру":
+				bot.HandleNewGame(botAPI, update.Message.Chat.ID, update.Message.From.ID, update.Message.From.UserName, &manager)
+				manager.Game.Mode = "dota"
+			case "[clash royale] создать игру":
+				bot.HandleNewGame(botAPI, update.Message.Chat.ID, update.Message.From.ID, update.Message.From.UserName, &manager)
+				manager.Game.Mode = "clash"
+			case "присоединиться к игре":
 				bot.HandleJoin(botAPI, update.Message, &manager)
-			case "/begin":
+			case "начать игру":
 				bot.HandleBegin(botAPI, update.Message.Chat.ID, &manager)
+			case "завершить игру":
+				bot.HandleEnd(botAPI, update.Message.Chat.ID, &manager)
 			}
 
 		}
